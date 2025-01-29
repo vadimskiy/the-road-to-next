f6e7503081e0537b1ddf5fabd3f181f48be19e33
Vadim Sapovsky <vadym.sapovskyi@wework.com>
2025-01-29 19:03:07 +0200

    add ticket status

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,11 +2,18 @@ import Link from "next/link";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
+const TICKET_ICONS = {
+    OPEN: 'O',
+    DONE: 'X',
+    IN_PROGRESS: '>',
+  };
+
 const TicketsPage = () => {
     return (
       <div>
         {initialTickets.map((ticket) => (
             <div key={ticket.id}>
+                <div>{TICKET_ICONS[ticket.status]}</div>
                 <h2 className="text-lg">{ticket.title}</h2>
                 
                 <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
