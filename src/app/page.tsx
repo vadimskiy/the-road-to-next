18526305e286aa151295974f3064c6871c819a62
Vadim Sapovsky <vadym.sapovskyi@wework.com>
2025-01-30 10:41:21 +0200

    add styles for tickets page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,20 @@ import { ticketsPath } from '@/paths';
 
 const HomePage = () => {
   return (
-    <><h2 className="text-lg">Home Page</h2><Link href={ticketsPath()} className="underline">Go to tickets</Link></>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Home</h2>
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
+      
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="text-sm underline">
+          Go to Tickets
+        </Link>
+      </div>
+    </div>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,7 +12,17 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        "fade-from-top": {
+          from: { opacity: "0", transform: "translateY(-16px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-from-top": "fade-from-top 0.5s ease-out",
+      },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
