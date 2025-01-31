00c7000a47d0a28cd27998b7912b70ffcdce54ef
vadym_sapovsky <v.sapovskiy@gmail.com>
2025-01-31 15:29:30 +0200

    update theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
           <div>
             <Link
               href={ticketsPath()}
-              className={buttonVariants({ variant: "outline" })}
+              className={buttonVariants({ variant: "default" })}
             >
               Tickets
             </Link>
