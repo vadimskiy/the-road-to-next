14da319da60dc44b081e502b6c888bb176d5a5a9
vadym_sapovsky <v.sapovskiy@gmail.com>
2025-02-02 17:09:38 +0200

    mimic fetching data for client component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,14 +1,30 @@
+'use client';
+
+import { useEffect, useState } from "react";
 import { Heading } from "@/components/custom/heading";
-import { initialTickets } from "@/data";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
+import { getTickets } from "@/features/ticket/queries/get-tickets";
+import { Ticket } from "@/features/ticket/types";
 
 const TicketsPage = () => {
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      const result = await getTickets();
+
+      setTickets(result);
+    };
+
+    fetchTickets();
+  }, []);
+
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-        {initialTickets.map((ticket) => (
+        {tickets.map((ticket) => (
           <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
