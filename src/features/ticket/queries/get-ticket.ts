0a276bc2fa1b91304f929d29aa18aee2f8621fb6
vadym_sapovsky <v.sapovskiy@gmail.com>
2025-02-05 18:51:45 +0200

    add serveer queries

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,14 +1,9 @@
-import { initialTickets } from "@/data";
-import { Ticket } from "../types";
+import { prisma } from "@/lib/prisma";
 
-export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
-    await new Promise((resolve) => {
-        setTimeout(resolve, 2000);
-    });
-
-    const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
-
-    return new Promise((resolve) => {
-        resolve(ticket || null);
+export const getTicket = async (id: string) => {
+    return await prisma.ticket.findUnique({
+        where: {
+            id,
+        },
     });
 };
