9ccb7df6675111b420ff2514e0423dbe284ab81e
vadym_sapovsky <v.sapovskiy@gmail.com>
2025-02-10 18:09:33 +0200

    add submit button with the state

---
+++ src/components/custom/form/submit-button.tsx
@@ -0,0 +1,20 @@
+import { LucideLoaderCircle } from "lucide-react";
+import { useFormStatus } from "react-dom";
+import { Button } from "@/components/ui/button";
+
+type SubmitButtonProps = {
+    label: string;
+};
+
+const SubmitButton = ({ label }: SubmitButtonProps) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit">
+      {pending && <LucideLoaderCircle className="mr-2 h-4 w-4 animate-spin" />}
+      {label}
+    </Button>
+  );
+};
+
+export { SubmitButton };
--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,26 +1,31 @@
+"use client";
+
 import { Ticket } from "@prisma/client";
-import { Button } from "@/components/ui/button";
+import { SubmitButton } from "@/components/custom/form/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { upsertTicket } from "../actions/upsert-ticket";
 
 type TicketUpsertFormProps = {
-    ticket?: Ticket;
+  ticket?: Ticket;
 };
 
 const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
-    return (
-        <form action={upsertTicket.bind(null, ticket?.id)} className="flex flex-col gap-y-2">
-            <Label htmlFor="title">Title</Label>
-            <Input id="title" name="title" type="text" defaultValue={ticket?.title} />
+  return (
+    <form
+      action={upsertTicket.bind(null, ticket?.id)}
+      className="flex flex-col gap-y-2"
+    >
+      <Label htmlFor="title">Title</Label>
+      <Input id="title" name="title" type="text" defaultValue={ticket?.title} />
 
-            <Label htmlFor="content">Content</Label>
-            <Textarea id="content" name="content" defaultValue={ticket?.content} />
+      <Label htmlFor="content">Content</Label>
+      <Textarea id="content" name="content" defaultValue={ticket?.content} />
 
-            <Button type="submit">{ticket ? 'Edit' : 'Create'}</Button>
-        </form>
-    );
+      <SubmitButton label={ticket ? "Edit" : "Create"} />
+    </form>
+  );
 };
 
 export { TicketUpsertForm };
