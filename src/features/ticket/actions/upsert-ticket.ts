1ade524737cd50be1a1ad55d2af6c4359523ec1a
vadym_sapovsky <v.sapovskiy@gmail.com>
2025-02-11 17:25:04 +0200

    add error handling

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
+import { ActionState, fromErrorToActionState } from "@/components/custom/form/utils/to-action-state";
 import { prisma } from "@/lib/prisma";
 import { ticketPath, ticketsPath } from "@/paths";
 
@@ -11,7 +12,7 @@ const upsertTicketSchema = z.object({
     content: z.string().min(1).max(1024),
 });
 
-export const upsertTicket = async (id: string | undefined, _actionState: { message: string }, formData: FormData) => {
+export const upsertTicket = async (id: string | undefined, _actionState: ActionState, formData: FormData) => {
     try {
         const data = upsertTicketSchema.parse({
             title: formData.get('title') as string,
@@ -26,7 +27,7 @@ export const upsertTicket = async (id: string | undefined, _actionState: { messa
             create: data,
         });
     } catch (error) {
-        return { message: "Something went wrong" };
+        return fromErrorToActionState(error, formData);
     }
 
     revalidatePath(ticketsPath());
--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -26,10 +26,10 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
       className="flex flex-col gap-y-2"
     >
       <Label htmlFor="title">Title</Label>
-      <Input id="title" name="title" type="text" defaultValue={ticket?.title} />
+      <Input id="title" name="title" type="text" defaultValue={(actionState.payload?.get('title') as string) ?? ticket?.title} />
 
       <Label htmlFor="content">Content</Label>
-      <Textarea id="content" name="content" defaultValue={ticket?.content} />
+      <Textarea id="content" name="content" defaultValue={(actionState.payload?.get('content') as string) ?? ticket?.content} />
 
       <SubmitButton label={ticket ? "Edit" : "Create"} />
 
